1e9d1c0875b22d198fdb9925e402721021c8b06a
dakshinamurthy <dakshinamurthy13@gmail.com>
2021-07-15 13:06:24 +0530

    Responsive Layouts

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,10 +11,9 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 300,
-      child: transactions.isEmpty
-          ? Column(
+    return transactions.isEmpty
+        ? LayoutBuilder(builder: (ctx, constraints) {
+            return Column(
               children: <Widget>[
                 Text(
                   "No Transactions added yet!",
@@ -24,92 +23,98 @@ class TransactionList extends StatelessWidget {
                   height: 50,
                 ),
                 Container(
-                    height: 200,
+                    height: constraints.maxHeight * 0.6,
                     child: Image.asset(
                       'assets/images/waiting.png',
                       fit: BoxFit.cover,
                     )),
               ],
-            )
-          : ListView.builder(
-              itemBuilder: (context, index) {
-                // return Card(
-                //   child: Row(
-                //     children: <Widget>[
-                //       Container(
-                //         margin: EdgeInsets.symmetric(
-                //           vertical: 10,
-                //           horizontal: 15,
-                //         ),
-                //         padding: EdgeInsets.all(10),
-                //         decoration: BoxDecoration(
-                //           border: Border.all(
-                //             color: Theme.of(context).primaryColor,
-                //             width: 2,
-                //           ),
-                //         ),
-                //         child: Text(
-                //           '\$${transactions[index].amount.toStringAsFixed(2)}',
-                //           style: TextStyle(
-                //             fontWeight: FontWeight.bold,
-                //             fontSize: 20,
-                //             color: Theme.of(context).primaryColor,
-                //           ),
-                //         ),
-                //       ),
-                //       Column(
-                //         children: <Widget>[
-                //           Text(
-                //             transactions[index].title,
-                //             style: Theme.of(context).textTheme.headline6,
-                //           ),
-                //           Text(
-                //             DateFormat.yMMMd().format(transactions[index].date),
-                //             style: TextStyle(
-                //               color: Colors.grey,
-                //             ),
-                //           ),
-                //         ],
-                //         crossAxisAlignment: CrossAxisAlignment.start,
-                //       )
-                //     ],
-                //   ),
-                // );
-                return Card(
-                  margin: EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 5,
+            );
+          })
+        : ListView.builder(
+            itemBuilder: (context, index) {
+              // return Card(
+              //   child: Row(
+              //     children: <Widget>[
+              //       Container(
+              //         margin: EdgeInsets.symmetric(
+              //           vertical: 10,
+              //           horizontal: 15,
+              //         ),
+              //         padding: EdgeInsets.all(10),
+              //         decoration: BoxDecoration(
+              //           border: Border.all(
+              //             color: Theme.of(context).primaryColor,
+              //             width: 2,
+              //           ),
+              //         ),
+              //         child: Text(
+              //           '\$${transactions[index].amount.toStringAsFixed(2)}',
+              //           style: TextStyle(
+              //             fontWeight: FontWeight.bold,
+              //             fontSize: 20,
+              //             color: Theme.of(context).primaryColor,
+              //           ),
+              //         ),
+              //       ),
+              //       Column(
+              //         children: <Widget>[
+              //           Text(
+              //             transactions[index].title,
+              //             style: Theme.of(context).textTheme.headline6,
+              //           ),
+              //           Text(
+              //             DateFormat.yMMMd().format(transactions[index].date),
+              //             style: TextStyle(
+              //               color: Colors.grey,
+              //             ),
+              //           ),
+              //         ],
+              //         crossAxisAlignment: CrossAxisAlignment.start,
+              //       )
+              //     ],
+              //   ),
+              // );
+              return Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 5,
+                ),
+                elevation: 5,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    child: Padding(
+                      padding: const EdgeInsets.all(6.0),
+                      child: FittedBox(
+                        child: Text('\$${transactions[index].amount}'),
+                      ),
+                    ),
+                    radius: 30,
                   ),
-                  elevation: 5,
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      child: Padding(
-                        padding: const EdgeInsets.all(6.0),
-                        child: FittedBox(
-                          child: Text('\$${transactions[index].amount}'),
+                  title: Text(
+                    transactions[index].title,
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  subtitle: Text(
+                    DateFormat.yMMMd().format(transactions[index].date),
+                  ),
+                  trailing: MediaQuery.of(context).size.width > 360
+                      ? FlatButton.icon(
+                          onPressed: () => deleteTx(transactions[index].id),
+                          icon: Icon(Icons.delete),
+                          textColor: Theme.of(context).errorColor,
+                          label: Text('Delete'))
+                      : IconButton(
+                          icon: Icon(
+                            Icons.delete,
+                            color: Theme.of(context).errorColor,
+                          ),
+                          onPressed: () => deleteTx(transactions[index].id),
                         ),
-                      ),
-                      radius: 30,
-                    ),
-                    title: Text(
-                      transactions[index].title,
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    subtitle: Text(
-                      DateFormat.yMMMd().format(transactions[index].date),
-                    ),
-                    trailing: IconButton(
-                      icon: Icon(
-                        Icons.delete,
-                        color: Theme.of(context).errorColor,
-                      ),
-                      onPressed: () => deleteTx(transactions[index].id),
-                    ),
-                  ),
-                );
-              },
-              itemCount: transactions.length,
-            ),
-    );
+                ),
+              );
+            },
+            itemCount: transactions.length,
+          );
   }
 }
